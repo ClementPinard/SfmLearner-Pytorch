7b6e6b3ac30a4eaad468e8278f0f722025d4a4f7
Clément Pinard <clement.pinard@parrot.com>
2017-11-22 15:39:28 +0100

    add no mask option for PoseExpNet

--- models/PoseExpNet.py
+++ models/PoseExpNet.py
@@ -19,9 +19,10 @@ def upconv(in_planes, out_planes):
 
 class PoseExpNet(nn.Module):
 
-    def __init__(self, nb_ref_imgs=2):
+    def __init__(self, nb_ref_imgs=2, output_exp=False):
         super(PoseExpNet, self).__init__()
         self.nb_ref_imgs = nb_ref_imgs
+        self.output_exp = output_exp
 
         conv_planes = [16, 32, 64, 128, 256, 256, 256]
         self.conv1 = conv( 3*(1+self.nb_ref_imgs), conv_planes[0], kernel_size=7)
@@ -34,17 +35,18 @@ class PoseExpNet(nn.Module):
 
         self.pose_pred = nn.Conv2d(conv_planes[6], 6*self.nb_ref_imgs, kernel_size=1, padding=0)
 
-        upconv_planes = [256, 128, 64, 32, 16]
-        self.upconv5 = upconv( conv_planes[4], upconv_planes[0])
-        self.upconv4 = upconv( upconv_planes[0], upconv_planes[1])
-        self.upconv3 = upconv( upconv_planes[1], upconv_planes[2])
-        self.upconv2 = upconv( upconv_planes[2], upconv_planes[3])
-        self.upconv1 = upconv( upconv_planes[3], upconv_planes[4])
+        if self.output_exp:
+            upconv_planes = [256, 128, 64, 32, 16]
+            self.upconv5 = upconv( conv_planes[4], upconv_planes[0])
+            self.upconv4 = upconv( upconv_planes[0], upconv_planes[1])
+            self.upconv3 = upconv( upconv_planes[1], upconv_planes[2])
+            self.upconv2 = upconv( upconv_planes[2], upconv_planes[3])
+            self.upconv1 = upconv( upconv_planes[3], upconv_planes[4])
 
-        self.predict_mask4 = nn.Conv2d(upconv_planes[1], self.nb_ref_imgs, kernel_size=3, padding=1)
-        self.predict_mask3 = nn.Conv2d(upconv_planes[2], self.nb_ref_imgs, kernel_size=3, padding=1)
-        self.predict_mask2 = nn.Conv2d(upconv_planes[3], self.nb_ref_imgs, kernel_size=3, padding=1)
-        self.predict_mask1 = nn.Conv2d(upconv_planes[4], self.nb_ref_imgs, kernel_size=3, padding=1)
+            self.predict_mask4 = nn.Conv2d(upconv_planes[1], self.nb_ref_imgs, kernel_size=3, padding=1)
+            self.predict_mask3 = nn.Conv2d(upconv_planes[2], self.nb_ref_imgs, kernel_size=3, padding=1)
+            self.predict_mask2 = nn.Conv2d(upconv_planes[3], self.nb_ref_imgs, kernel_size=3, padding=1)
+            self.predict_mask1 = nn.Conv2d(upconv_planes[4], self.nb_ref_imgs, kernel_size=3, padding=1)
 
     def init_weights(self):
         for m in self.modules():
@@ -65,21 +67,27 @@ class PoseExpNet(nn.Module):
         out_conv5 = self.conv5(out_conv4)
         out_conv6 = self.conv6(out_conv5)
         out_conv7 = self.conv7(out_conv6)
-
-        out_upconv5 = self.upconv5(  out_conv5)[:, :, 0:out_conv4.size(2), 0:out_conv4.size(3)]
-        out_upconv4 = self.upconv4(out_upconv5)[:, :, 0:out_conv3.size(2), 0:out_conv3.size(3)]
-        out_upconv3 = self.upconv3(out_upconv4)[:, :, 0:out_conv2.size(2), 0:out_conv2.size(3)]
-        out_upconv2 = self.upconv2(out_upconv3)[:, :, 0:out_conv1.size(2), 0:out_conv1.size(3)]
-        out_upconv1 = self.upconv1(out_upconv2)[:, :, 0:input.size(2), 0:input.size(3)]
         
         pose = self.pose_pred(out_conv7)
         pose = pose.mean(3).mean(2)
         pose = 0.01 * pose.view(pose.size(0), self.nb_ref_imgs, 6)
-        
-        exp_mask4 = nn.functional.sigmoid(self.predict_mask4(out_upconv4))
-        exp_mask3 = nn.functional.sigmoid(self.predict_mask3(out_upconv3))
-        exp_mask2 = nn.functional.sigmoid(self.predict_mask2(out_upconv2))
-        exp_mask1 = nn.functional.sigmoid(self.predict_mask1(out_upconv1))
+
+        if self.output_exp:
+            out_upconv5 = self.upconv5(  out_conv5)[:, :, 0:out_conv4.size(2), 0:out_conv4.size(3)]
+            out_upconv4 = self.upconv4(out_upconv5)[:, :, 0:out_conv3.size(2), 0:out_conv3.size(3)]
+            out_upconv3 = self.upconv3(out_upconv4)[:, :, 0:out_conv2.size(2), 0:out_conv2.size(3)]
+            out_upconv2 = self.upconv2(out_upconv3)[:, :, 0:out_conv1.size(2), 0:out_conv1.size(3)]
+            out_upconv1 = self.upconv1(out_upconv2)[:, :, 0:input.size(2), 0:input.size(3)]
+
+            exp_mask4 = nn.functional.sigmoid(self.predict_mask4(out_upconv4))
+            exp_mask3 = nn.functional.sigmoid(self.predict_mask3(out_upconv3))
+            exp_mask2 = nn.functional.sigmoid(self.predict_mask2(out_upconv2))
+            exp_mask1 = nn.functional.sigmoid(self.predict_mask1(out_upconv1))
+        else:
+            exp_mask4 = None
+            exp_mask3 = None
+            exp_mask2 = None
+            exp_mask1 = None
 
         if self.training:
             return [exp_mask1, exp_mask2, exp_mask3, exp_mask4], pose
