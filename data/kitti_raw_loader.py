52826c8ee002ff7d2ee0c73657fb1dd9ddb9384e
Clément Pinard <clement.pinard@parrot.com>
2018-01-10 16:05:47 +0100

    allow prepare data to save groudtruth for validation set

--- data/kitti_raw_loader.py
+++ data/kitti_raw_loader.py
@@ -1,6 +1,7 @@
 import numpy as np
 from path import Path
 import scipy.misc
+from collections import Counter
 
 
 class KittiRawLoader(object):
@@ -10,10 +11,15 @@ class KittiRawLoader(object):
                  img_height=128,
                  img_width=416,
                  min_speed=2,
-                 seq_length=3):
+                 get_gt=False):
         dir_path = Path(__file__).realpath().dirname()
         test_scene_file = dir_path/'test_scenes.txt'
-        static_frames_file = Path(static_frames_file)
+
+        self.from_speed = static_frames_file is None
+        if static_frames_file is not None:
+            static_frames_file = Path(static_frames_file)
+            self.collect_static_frames(static_frames_file)
+
         with open(test_scene_file, 'r') as f:
             test_scenes = f.readlines()
         self.test_scenes = [t[:-1] for t in test_scenes]
@@ -23,8 +29,8 @@ class KittiRawLoader(object):
         self.cam_ids = ['02', '03']
         self.date_list = ['2011_09_26', '2011_09_28', '2011_09_29', '2011_09_30', '2011_10_03']
         self.min_speed = min_speed
+        self.get_gt = get_gt
         self.collect_train_folders()
-        self.collect_static_frames(static_frames_file)
 
     def collect_static_frames(self, static_frames_file):
         with open(static_frames_file, 'r') as f:
@@ -57,37 +63,45 @@ class KittiRawLoader(object):
                 speed = metadata[8:11]
                 scene_data['speed'].append(speed)
                 scene_data['frame_id'].append('{:010d}'.format(n))
-            res = self.load_image(scene_data, 0)
-            if res is None:
+            sample = self.load_image(scene_data, 0)
+            if sample is None:
                 return []
-            scene_data['intrinsics'] = self.get_intrinsics(scene_data, res[1], res[2])
+            scene_data['P_rect'] = self.get_P_rect(scene_data, sample[1], sample[2])
+            scene_data['intrinsics'] = scene_data['P_rect'][:,:3]
+
             train_scenes.append(scene_data)
         return train_scenes
 
-    def get_scene_imgs(self, scene_data, from_speed=False):
-        if from_speed:
+    def get_scene_imgs(self, scene_data):
+        def construct_sample(scene_data, i, frame_id):
+            sample = [self.load_image(scene_data, i)[0], frame_id]
+            if self.get_gt:
+                sample.append(self.generate_depth_map(scene_data, i))
+            return sample
+
+        if self.from_speed:
             cum_speed = np.zeros(3)
             for i, speed in enumerate(scene_data['speed']):
                 cum_speed += speed
                 speed_mag = np.linalg.norm(cum_speed)
                 if speed_mag > self.min_speed:
-                    yield self.load_image(scene_data, i)[0], scene_data['frame_id'][i]
+                    frame_id = scene_data['frame_id'][i]
+                    yield construct_sample(scene_data, i, frame_id)
                     cum_speed *= 0
         else:  # from static frame file
             drive = str(scene_data['dir'].name)
             for (i,frame_id) in enumerate(scene_data['frame_id']):
                 if (drive not in self.static_frames.keys()) or (frame_id not in self.static_frames[drive]):
-                    yield self.load_image(scene_data, i)[0], frame_id
+                    yield construct_sample(scene_data, i, frame_id)
 
-    def get_intrinsics(self, scene_data, zoom_x, zoom_y):
+    def get_P_rect(self, scene_data, zoom_x, zoom_y):
         calib_file = scene_data['dir'].parent/'calib_cam_to_cam.txt'
 
         filedata = self.read_raw_calib_file(calib_file)
         P_rect = np.reshape(filedata['P_rect_' + scene_data['cid']], (3, 4))
-        intrinsics = P_rect[:, :3]
-        intrinsics[0] *= zoom_x
-        intrinsics[1] *= zoom_y
-        return intrinsics
+        P_rect[0] *= zoom_x
+        P_rect[1] *= zoom_y
+        return P_rect
 
     def load_image(self, scene_data, tgt_idx):
         img_file = scene_data['dir']/'image_{}'.format(scene_data['cid'])/'data'/scene_data['frame_id'][tgt_idx]+'.png'
@@ -114,3 +128,58 @@ class KittiRawLoader(object):
                 except ValueError:
                         pass
         return data
+
+    def generate_depth_map(self, scene_data, tgt_idx):
+        # compute projection matrix velodyne->image plane
+
+        def sub2ind(matrixSize, rowSub, colSub):
+            m, n = matrixSize
+            return rowSub * (n-1) + colSub - 1
+
+        R_cam2rect = np.eye(4)
+
+        calib_dir = scene_data['dir'].parent
+        cam2cam = self.read_raw_calib_file(calib_dir/'calib_cam_to_cam.txt')
+        velo2cam = self.read_raw_calib_file(calib_dir/'calib_velo_to_cam.txt')
+        velo2cam = np.hstack((velo2cam['R'].reshape(3,3), velo2cam['T'][..., np.newaxis]))
+        velo2cam = np.vstack((velo2cam, np.array([0, 0, 0, 1.0])))
+        P_rect = scene_data['P_rect']
+        R_cam2rect[:3,:3] = cam2cam['R_rect_00'].reshape(3,3)
+
+        P_velo2im = np.dot(np.dot(P_rect, R_cam2rect), velo2cam)
+
+        velo_file_name = scene_data['dir']/'velodyne_points'/'data'/'{}.bin'.format(scene_data['frame_id'][tgt_idx])
+
+        # load velodyne points and remove all behind image plane (approximation)
+        # each row of the velodyne data is forward, left, up, reflectance
+        velo = np.fromfile(velo_file_name, dtype=np.float32).reshape(-1, 4)
+        velo[:,3] = 1
+        velo = velo[velo[:, 0] >= 0, :]
+
+        # project the points to the camera
+        velo_pts_im = np.dot(P_velo2im, velo.T).T
+        velo_pts_im[:, :2] = velo_pts_im[:,:2] / velo_pts_im[:,-1:]
+
+        # check if in bounds
+        # use minus 1 to get the exact same value as KITTI matlab code
+        velo_pts_im[:, 0] = np.round(velo_pts_im[:,0]) - 1
+        velo_pts_im[:, 1] = np.round(velo_pts_im[:,1]) - 1
+
+        val_inds = (velo_pts_im[:, 0] >= 0) & (velo_pts_im[:, 1] >= 0)
+        val_inds = val_inds & (velo_pts_im[:,0] < self.img_width) & (velo_pts_im[:,1] < self.img_height)
+        velo_pts_im = velo_pts_im[val_inds, :]
+
+        # project to image
+        depth = np.zeros((self.img_height, self.img_width)).astype(np.float32)
+        depth[velo_pts_im[:, 1].astype(np.int), velo_pts_im[:, 0].astype(np.int)] = velo_pts_im[:, 2]
+
+        # find the duplicate points and choose the closest depth
+        inds = sub2ind(depth.shape, velo_pts_im[:, 1], velo_pts_im[:, 0])
+        dupe_inds = [item for item, count in Counter(inds).items() if count > 1]
+        for dd in dupe_inds:
+            pts = np.where(inds == dd)[0]
+            x_loc = int(velo_pts_im[pts[0], 0])
+            y_loc = int(velo_pts_im[pts[0], 1])
+            depth[y_loc, x_loc] = velo_pts_im[pts, 2].min()
+        depth[depth < 0] = 0
+        return depth
--- data/prepare_train_data.py
+++ data/prepare_train_data.py
@@ -13,6 +13,8 @@ parser.add_argument("--dataset-format", type=str, required=True, choices=["kitti
 parser.add_argument("--static-frames", default=None,
                     help="list of imgs to discard for being static, if not set will discard them based on speed \
                     (careful, on KITTI some frames have incorrect speed)")
+parser.add_argument("--with-gt", action='store_true',
+                    help="If available (e.g. with KITTI), will store ground truth along with images, for validation")
 parser.add_argument("--dump-root", type=str, required=True, help="Where to dump the data")
 parser.add_argument("--height", type=int, default=128, help="image height")
 parser.add_argument("--width", type=int, default=416, help="image width")
@@ -36,9 +38,14 @@ def dump_example(scene):
         with open(dump_cam_file, 'w') as f:
             f.write('%f,0.,%f,0.,%f,%f,0.,0.,1.' % (fx, cx, fy, cy))
 
-        for img, frame_nb in data_loader.get_scene_imgs(scene_data):
+        for sample in data_loader.get_scene_imgs(scene_data):
+            assert(len(sample) >= 2)
+            img, frame_nb = sample[0], sample[1]
             dump_img_file = dump_dir/'{}.jpg'.format(frame_nb)
             scipy.misc.imsave(dump_img_file, img)
+            if len(sample) == 3:
+                dump_depth_file = dump_dir/'{}.npy'.format(frame_nb)
+                np.save(dump_depth_file, sample[2])
 
         if len(dump_dir.files('*.jpg')) < 3:
             dump_dir.rmtree()
@@ -55,7 +62,8 @@ def main():
         data_loader = KittiRawLoader(args.dataset_dir,
                                      static_frames_file=args.static_frames,
                                      img_height=args.height,
-                                     img_width=args.width)
+                                     img_width=args.width,
+                                     get_gt=args.with_gt)
 
     if args.dataset_format == 'cityscapes':
         from cityscapes_loader import cityscapes_loader
@@ -76,6 +84,9 @@ def main():
                     vf.write('{}\n'.format(s.name))
                 else:
                     tf.write('{}\n'.format(s.name))
+                    # remove useless groundtruth data for training comment if you don't want to erase it
+                    for gt_file in s.files('*.npy'):
+                        gt_file.remove_p()
 
 
 if __name__ == '__main__':
