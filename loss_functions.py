1d828bc6db36283ee56649e27dc38c47dd8f085f
Clément Pinard <clement.pinard@parrot.com>
2018-01-10 18:30:20 +0100

    add valid errors functions

--- loss_functions.py
+++ loss_functions.py
@@ -71,19 +71,43 @@ def smooth_loss(pred_disp):
         dx2, dxdy = gradient(dx)
         dydx, dy2 = gradient(dy)
         loss += (dx2.abs().mean() + dxdy.abs().mean() + dydx.abs().mean() + dy2.abs().mean())*weight
-        weight /= 2
+        weight /= 2.83  # 2sqrt(2)
     return loss
 
 
-def mean_loss(pred_disp, target_mean=5):
-    if type(pred_disp) not in [tuple, list]:
-        pred_disp = [pred_disp]
+def compute_errors(gt, pred, crop=True):
+    abs_diff, abs_rel, sq_rel, a1, a2, a3 = 0,0,0,0,0,0
+    batch_size = gt.size(0)
 
-    loss = 0
-    weight = 1.
+    '''
+    crop used by Garg ECCV16 to reprocude Eigen NIPS14 results
+    construct a mask of False values, with the same size as target
+    and then set to True values inside the crop
+    '''
+    if crop:
+        crop_mask = gt[0] != gt[0]
+        y1,y2 = int(0.40810811 * gt.size(1)), int(0.99189189 * gt.size(1))
+        x1,x2 = int(0.03594771 * gt.size(2)), int(0.96405229 * gt.size(2))
+        crop_mask[y1:y2,x1:x2] = 1
 
-    for scaled_disp in pred_disp:
+    for current_gt, current_pred in zip(gt, pred):
+        valid = (current_gt > 0) & (current_gt < 80)
+        if crop:
+            valid = valid & crop_mask
 
-        loss += weight * (scaled_disp.mean()/target_mean - 1)
-        weight /= 2
-    return loss
+        valid_gt = current_gt[valid]
+        valid_pred = current_pred[valid].clamp(1e-3, 80)
+
+        valid_pred = valid_pred * torch.median(valid_gt)/torch.median(valid_pred)
+
+        thresh = torch.max((valid_gt / valid_pred), (valid_pred / valid_gt))
+        a1 += (thresh < 1.25).float().mean()
+        a2 += (thresh < 1.25 ** 2).float().mean()
+        a3 += (thresh < 1.25 ** 3).float().mean()
+
+        abs_diff += torch.mean(torch.abs(valid_gt - valid_pred))
+        abs_rel += torch.mean(torch.abs(valid_gt - valid_pred) / valid_gt)
+
+        sq_rel += torch.mean(((valid_gt - valid_pred)**2) / valid_gt)
+
+    return [metric / batch_size for metric in [abs_diff, abs_rel, sq_rel, a1, a2, a3]]
