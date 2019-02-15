fix scale ratio for test disp * Now speed is integrated to get displacement magnitude * GPS can still be used if specified * Will warn user if oxts say that precision is bad (>2m) * Also fix a bug for still box displacements

kitti_eval/depth_evaluation_utils.py
@@ -5,13 +5,19 @@ from collections import Counter
 from path import Path
 from scipy.misc import imread
 from tqdm import tqdm
+import datetime
 
 
 class test_framework_KITTI(object):
-    def __init__(self, root, test_files, seq_length=3, min_depth=1e-3, max_depth=100, step=1):
+    def __init__(self, root, test_files, seq_length=3, min_depth=1e-3, max_depth=100, step=1, use_gps=True):
         self.root = root
         self.min_depth, self.max_depth = min_depth, max_depth
-        self.calib_dirs, self.gt_files, self.img_files, self.displacements, self.cams = read_scene_data(self.root, test_files, seq_length, step)
+        self.use_gps = use_gps
+        self.calib_dirs, self.gt_files, self.img_files, self.displacements, self.cams = read_scene_data(self.root,
+                                                                                                        test_files,
+                                                                                                        seq_length,
+                                                                                                        step,
+                                                                                                        self.use_gps)
 
     def __getitem__(self, i):
         tgt = imread(self.img_files[i][0]).astype(np.float32)
@@ -20,7 +26,7 @@ class test_framework_KITTI(object):
                 'ref': [imread(img).astype(np.float32) for img in self.img_files[i][1]],
                 'path':self.img_files[i][0],
                 'gt_depth': depth,
-                'displacement': np.array(self.displacements[i]),
+                'displacements': np.array(self.displacements[i]),
                 'mask': generate_mask(depth, self.min_depth, self.max_depth)
                 }
 
@@ -49,27 +55,61 @@ def getXYZ(lat, lon, alt):
     return t
 
 
-def get_displacements(oxts_root, indices, tgt_index):
-    """gets mean displacement magntidue between middle frame and other frames, this is, to a scaling factor
+def get_displacements_from_GPS(root, date, scene, indices, tgt_index, precision_warning_threshold=2):
+    """gets displacement magntidues between middle frame and other frames, this is, to a scaling factor
     the mean output PoseNet should have for translation. Since the scaling is the same factor for depth maps and
     for translations, it will be used to determine how much predicted depth should be multiplied to."""
+
     first_pose = None
-    displacement = 0
+    displacements = []
+    oxts_root = root/date/scene/'oxts'
     if len(indices) == 0:
         return 0
     reordered_indices = [indices[tgt_index]] + [*indices[:tgt_index]] + [*indices[tgt_index + 1:]]
+    already_warned = False
     for index in reordered_indices:
         oxts_data = np.genfromtxt(oxts_root/'data'/'{:010d}.txt'.format(index))
+
+        if not already_warned:
+            position_precision = oxts_data[23]
+            if position_precision > precision_warning_threshold:
+                print("Warning for scene {} frame {} : bad position precision from oxts ({:.2f}m). "
+                      "You might want to get displacements from speed".format(scene, index, position_precision))
+            already_warned = True
+
         lat, lon, alt = oxts_data[:3]
         pose = getXYZ(lat, lon, alt)
         if first_pose is None:
             first_pose = pose
         else:
-            displacement += np.linalg.norm(pose - first_pose)
-    return displacement / max(len(indices - 1), 1)
+            displacements.append(np.linalg.norm(pose - first_pose))
+    return displacements
 
 
-def read_scene_data(data_root, test_list, seq_length=3, step=1):
+def get_displacements_from_speed(root, date, scene, indices, tgt_index):
+    """get displacement magnitudes by integrating over speed values.
+    Might be a good alternative if the GPS is not good enough"""
+    if len(indices) == 0:
+        return []
+    oxts_root = root/date/scene/'oxts'
+    with open(oxts_root/'timestamps.txt') as f:
+        timestamps = np.array([datetime.datetime.strptime(ts[:-3], "%Y-%m-%d %H:%M:%S.%f").timestamp() for ts in f.read().splitlines()])
+    speeds = np.zeros((len(indices), 3))
+    for i, index in enumerate(indices):
+        oxts_data = np.genfromtxt(oxts_root/'data'/'{:010d}.txt'.format(index))
+        speeds[i] = oxts_data[[6,7,10]]
+    displacements = np.zeros((len(indices), 3))
+    # Perform the integration operation, using trapezoidal method
+    for i0, (i1, i2) in enumerate(zip(indices, indices[1:])):
+        displacements[i0 + 1] = displacements[i0] + 0.5*(speeds[i0] + speeds[i0 + 1]) * (timestamps[i1] - timestamps[i2])
+    # Set the origin of displacements at tgt_index
+    displacements -= displacements[tgt_index]
+    # Finally, get the displacement magnitude relative to tgt and discard the middle value (which is supposed to be 0)
+    displacements_mag = np.linalg.norm(displacements, axis=1)
+    return np.concatenate([displacements_mag[:tgt_index], displacements_mag[tgt_index + 1:]])
+
+
+def read_scene_data(data_root, test_list, seq_length=3, step=1, use_gps=True):
     data_root = Path(data_root)
     gt_files = []
     calib_dirs = []
@@ -96,10 +136,14 @@ def read_scene_data(data_root, test_list, seq_length=3, step=1):
             calib_dirs.append(data_root/date)
             im_files.append([tgt_img_path,ref_imgs_path])
             cams.append(int(cam_id[-2:]))
-            displacements.append(get_displacements(data_root/date/scene/'oxts', ref_indices, demi_length))
+
+            args = (data_root, date, scene, ref_indices, demi_length)
+            if use_gps:
+                displacements.append(get_displacements_from_GPS(*args))
+            else:
+                displacements.append(get_displacements_from_speed(*args))
         else:
             print('{} missing'.format(tgt_img_path))
-    # print(num_probs, 'files missing')
 
     return calib_dirs, gt_files, im_files, displacements, cams
 

stillbox_eval/depth_evaluation_utils.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 
 class test_framework_stillbox(object):
-    def __init__(self, root, test_files, seq_length=3, min_depth=1e-3, max_depth=80, step=1):
+    def __init__(self, root, test_files, seq_length=3, min_depth=1e-3, max_depth=80, step=1, **kwargs):
         self.root = root
         self.min_depth, self.max_depth = min_depth, max_depth
         self.gt_files, self.img_files, self.displacements = read_scene_data(self.root, test_files, seq_length, step)
@@ -18,7 +18,7 @@ class test_framework_stillbox(object):
                 'ref': [imread(img).astype(np.float32) for img in self.img_files[i][1]],
                 'path':self.img_files[i][0],
                 'gt_depth': depth,
-                'displacement': np.array(self.displacements[i]),
+                'displacements': np.array(self.displacements[i]),
                 'mask': generate_mask(depth, self.min_depth, self.max_depth)
                 }
 
@@ -29,14 +29,10 @@ class test_framework_stillbox(object):
 def get_displacements(scene, middle_index, ref_indices):
     assert(all(i < scene['length'] and i >= 0 for i in ref_indices)), str(ref_indices)
     atomic_movement = np.linalg.norm(scene['speed'])*scene['time_step']
-    cum_speed = 0
     """in Still box, movements are rectilinear so magnitude adds up.
     I mean, this is very convenient, I wonder who is the genius who came with such a dataset"""
-    for i,index in enumerate(ref_indices):
-        if index != middle_index:
-            cum_speed += atomic_movement * abs(index - middle_index)
-
-    return cum_speed/max(len(ref_indices) - 1, 1)
+    displacements = np.abs(atomic_movement * (np.array(ref_indices) - ref_indices[middle_index]))
+    return [*displacements[:middle_index], *displacements[middle_index+1:]]
 
 
 def read_scene_data(data_root, test_list, seq_length=3, step=1):

test_disp.py
@@ -25,6 +25,8 @@ parser.add_argument("--dataset-list", default=None, type=str, help="Dataset list
 parser.add_argument("--output-dir", default=None, type=str, help="Output directory for saving predictions in a big 3D numpy file")
 
 parser.add_argument("--gt-type", default='KITTI', type=str, help="GroundTruth data type", choices=['npy', 'png', 'KITTI', 'stillbox'])
+parser.add_argument("--gps", '-g', action='store_true',
+                    help='if selected, will get displacement from GPS for KITTI. Otherwise, will integrate speed')
 parser.add_argument("--img-exts", default=['png', 'jpg', 'bmp'], nargs='*', type=str, help="images extensions to glob")
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -46,7 +48,7 @@ def main():
     if args.pretrained_posenet is None:
         print('no PoseNet specified, scale_factor will be determined by median ratio, which is kiiinda cheating\
             (but consistent with original paper)')
-        seq_length = 0
+        seq_length = 1
     else:
         weights = torch.load(args.pretrained_posenet)
         seq_length = int(weights['state_dict']['conv1.0.weight'].size(1)/3)
@@ -60,10 +62,12 @@ def main():
     else:
         test_files = [file.relpathto(dataset_dir) for file in sum([dataset_dir.files('*.{}'.format(ext)) for ext in args.img_exts], [])]
 
-    framework = test_framework(dataset_dir, test_files, seq_length, args.min_depth, args.max_depth)
+    framework = test_framework(dataset_dir, test_files, seq_length,
+                               args.min_depth, args.max_depth,
+                               use_gps=args.gps)
 
     print('{} files to test'.format(len(test_files)))
-    errors = np.zeros((2, 7, len(test_files)), np.float32)
+    errors = np.zeros((2, 9, len(test_files)), np.float32)
     if args.output_dir is not None:
         output_dir = Path(args.output_dir)
         output_dir.makedirs_p()
@@ -107,31 +111,31 @@ def main():
             pred_depth_zoomed = pred_depth_zoomed[sample['mask']]
             gt_depth = gt_depth[sample['mask']]
 
-        if seq_length > 0:
+        if seq_length > 1:
             # Reorganize ref_imgs : tgt is middle frame but not necessarily the one used in DispNetS
             # (in case sample to test was in end or beginning of the image sequence)
             middle_index = seq_length//2
             tgt = ref_imgs[middle_index]
             reorganized_refs = ref_imgs[:middle_index] + ref_imgs[middle_index + 1:]
             _, poses = pose_net(tgt, reorganized_refs)
-            mean_displacement_magnitude = poses[0,:,:3].norm(2,1).mean().item()
+            displacement_magnitudes = poses[0,:,:3].norm(2,1).cpu().numpy()
 
-            scale_factor = sample['displacement'] / mean_displacement_magnitude
+            scale_factor = np.mean(sample['displacements'] / displacement_magnitudes)
             errors[0,:,j] = compute_errors(gt_depth, pred_depth_zoomed*scale_factor)
 
         scale_factor = np.median(gt_depth)/np.median(pred_depth_zoomed)
         errors[1,:,j] = compute_errors(gt_depth, pred_depth_zoomed*scale_factor)
 
     mean_errors = errors.mean(2)
-    error_names = ['abs_rel','sq_rel','rms','log_rms','a1','a2','a3']
+    error_names = ['abs_diff', 'abs_rel','sq_rel','rms','log_rms', 'abs_log', 'a1','a2','a3']
     if args.pretrained_posenet:
         print("Results with scale factor determined by PoseNet : ")
-        print("{:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}".format(*error_names))
-        print("{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}".format(*mean_errors[0]))
+        print("{:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}".format(*error_names))
+        print("{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}".format(*mean_errors[0]))
 
     print("Results with scale factor determined by GT/prediction ratio (like the original paper) : ")
-    print("{:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}".format(*error_names))
-    print("{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}".format(*mean_errors[1]))
+    print("{:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}, {:>10}".format(*error_names))
+    print("{:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}, {:10.4f}".format(*mean_errors[1]))
 
     if args.output_dir is not None:
         np.save(output_dir/'predictions.npy', predictions)
@@ -149,11 +153,14 @@ def compute_errors(gt, pred):
     rmse_log = (np.log(gt) - np.log(pred)) ** 2
     rmse_log = np.sqrt(rmse_log.mean())
 
+    abs_log = np.mean(np.abs(np.log(gt) - np.log(pred)))
+
     abs_rel = np.mean(np.abs(gt - pred) / gt)
+    abs_diff = np.mean(np.abs(gt - pred))
 
     sq_rel = np.mean(((gt - pred)**2) / gt)
 
-    return abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3
+    return abs_diff, abs_rel, sq_rel, rmse, rmse_log, abs_log, a1, a2, a3
 
 
 if __name__ == '__main__':