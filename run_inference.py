09545eb42841bff93c76623ac39f51568e97abf5
ksmith <ksmithnotfake@gmail.com>
2019-03-06 06:53:02 -0500

    minor image dump fix

--- run_inference.py
+++ run_inference.py
@@ -71,11 +71,11 @@ def main():
         file_name = '-'.join(file_path.splitall())
                                      
         if args.output_disp:
-            disp = (255*tensor2array(output, max_value=None, colormap='bone', channel_first=False)).astype(np.uint8)
+            disp = (255*tensor2array(output, max_value=None, colormap='bone')).astype(np.uint8)
             imsave(output_dir/'{}_disp{}'.format(file_name, file_ext), np.transpose(disp, (1,2,0)))
         if args.output_depth:
             depth = 1/output
-            depth = (255*tensor2array(depth, max_value=10, colormap='rainbow', channel_first=False)).astype(np.uint8)
+            depth = (255*tensor2array(depth, max_value=10, colormap='rainbow')).astype(np.uint8)
             imsave(output_dir/'{}_depth{}'.format(file_name, file_ext), np.transpose(depth, (1,2,0)))
 
 
